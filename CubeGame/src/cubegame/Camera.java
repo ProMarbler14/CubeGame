46c349ce1279df0ebc5e6e1ec046b2e6d7e95224
Glenn Smith <couleeapps@gmail.com>
2014-12-04 02:10:56 -0500

    Documentation so Jeff can figure out what I'm doing

--- CubeGame/src/cubegame/Camera.java
+++ CubeGame/src/cubegame/Camera.java
@@ -2,6 +2,7 @@
 // Camera.java
 //
 // Copyright (c) 2014 Jeff Hutchinson
+// Copyright (c) 2014 Glenn Smith
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -44,10 +45,24 @@ public class Camera {
 	 */
 	private Vector3f rotation;
 	
+	/**
+	 * Movement speed of the camera to be applied
+	 */
 	public static final float movementSpeed = 10.0f;
+	
+	/**
+	 * Rotation speed of the camera
+	 */
 	public static final float cameraSpeed = 0.01f;
 	
+	/**
+	 * Maximum pitch
+	 */
 	public static final float pitchMax = 1.4f;
+	
+	/**
+	 * Minimum pitch
+	 */
 	public static final float pitchMin = -1.55f;
 	
 	/**
@@ -78,8 +93,11 @@ public class Camera {
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 		
+		//Rotate the display by the rotatin matrix
 		Matrix4f r = getRotationMatrix();
 
+		//Cheap trick to get the float buffer to work, because using store() gives invalid transformations
+		// for some reasons.
 		float mat[] = {
 			r.m00, r.m10, r.m20, r.m30,
 			r.m01, r.m11, r.m21, r.m31,
@@ -87,11 +105,16 @@ public class Camera {
 			r.m03, r.m13, r.m23, r.m33
 		};
 		
+		//Create the actual float buffer because we love nio /s
 		FloatBuffer buffer = BufferUtils.createFloatBuffer(16);
 		buffer.put(mat);
+		//Fuck nio. If you don't rewind then glLoadMatrix tries to start loading from the end of the buffer.
 		buffer.rewind();
-		
+
+		//Load the actual matrix
 		glLoadMatrix(buffer);
+		
+		//Translate the matrix, not sure why Jeff inverted x, but this works
 		glTranslatef(-position.x, position.y, position.z);
 	}
 	
@@ -122,23 +145,11 @@ public class Camera {
 	}
 	
 	/**
-	 * Gets the matrix rotation of the camera
+	 * Gets the Euler rotation of the camera
 	 * @return the rotation
 	 */
 	public Vector3f getRotation() {
 		return rotation;
-	}
-	
-	public float getYaw() {
-		//TODO
-		return 0;
-//		return rotation.y;
-	}
-	
-	public float getPitch() {
-		//TODO
-		return 0;
-//		return rotation.x;
 	}
 	
 	/**
@@ -148,13 +159,20 @@ public class Camera {
 	 * @param z the amount to move in the Z direction
 	 */
 	public void applyMotion(float x, float y, float z) {
+		//Create a raw motion vector scaled by speed
 		Vector3f motion = (Vector3f) new Vector3f(x, y, z).scale(movementSpeed);
 		
+		//A matrix is used to rotate the motion vector to the camera's current orientation 
 		Matrix4f matrix = (Matrix4f) new Matrix4f().setIdentity();
+		
+		//Yaw is inverted because we're facing outwards (thanks to Jeff for explaining this to me)
 		Matrix4f.rotate(rotation.y, new Vector3f(0.0f, -1.0f, 0.0f), matrix, matrix);
 		Matrix4f.rotate(rotation.x, new Vector3f(1.0f, 0.0f, 0.0f), matrix, matrix);
+		
+		//Translate second
 		Matrix4f.translate(motion, matrix, matrix);
 		
+		//Cheap trick to get position from the matrix, as LWJGL doesn't actually have .position
 		position = Vector3f.add(position, new Vector3f(matrix.m30, matrix.m31, matrix.m32), null);
 	}
 	
@@ -167,12 +185,22 @@ public class Camera {
 		rotation.x += pitch * cameraSpeed;
 		rotation.y += -yaw * cameraSpeed;
 		
+		//Clamp pitch
 		rotation.x = Math.max(Math.min(rotation.x, pitchMax), pitchMin);
+		
+		//Clamp yaw
+		rotation.y %= Math.PI * 2.0f;
 	}
 	
+	/**
+	 * Gets the camera's rotation as a Matrix4f
+	 * @return A matrix representation of the camera's rotation
+	 */
 	public Matrix4f getRotationMatrix() {
 		Matrix4f r = new Matrix4f();
 		r.setIdentity();
+		
+		//Rotate by yaw, pitch
 		Matrix4f.rotate(rotation.y, new Vector3f(0.0f, 1.0f, 0.0f), r, r);
 		Matrix4f.rotate(rotation.x, new Vector3f(1.0f, 0.0f, 0.0f), r, r);
 		
