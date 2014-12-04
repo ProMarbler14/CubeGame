cf975b2cfe12525fca5ea619ad7aba69362dd284
Jeff Hutchinson <programmerjeff95@gmail.com>
2014-12-04 16:20:41 -0500

    don't you want credit G?

--- CubeGame/src/cubegame/World.java
+++ CubeGame/src/cubegame/World.java
@@ -2,6 +2,7 @@
 // World.java
 //
 // Copyright (c) 2014 Jeff Hutchinson
+// Copyright (c) 2014 Glenn Smith
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
